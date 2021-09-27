92f2b45405c62a2a57b23ca8261513c6c819637c
KChi <59391215+KagChi@users.noreply.github.com>
2021-09-27 19:20:07 +0700

    replit, thanks

--- node_modules/vuepress-theme-yuu/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-module.exports = {
-	extend: '@vuepress/theme-default',
-	plugins: [
-		require('./plugins/yuu.js'),
-	],
-}
--- node_modules/vuepress-theme-yuu/plugins/yuu.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-const path = require('path')
-
-module.exports = {
-	name: 'yuu-theme-plugin',
-	clientRootMixin: path.join(__dirname, 'lifecycle.js'),
-}
